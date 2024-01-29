3f1b92fa77587d71be6f3931bcca9b4fcfb69615
raphaeldonza <raphael.donza@gmail.com>
2024-01-29 08:50:06 -0300

    alteração

--- Contexts/ExoContext.cs
+++ Contexts/ExoContext.cs
@@ -18,16 +18,10 @@ namespace Exo.WebApi.Contexts
         {
             if (!optionsBuilder.IsConfigured)
             {
-                // Essa string de conexão depende da SUA máquina.
+                // String de conexão
                 optionsBuilder.UseSqlServer("Server = localhost\\SQLEXPRESS;"
                 + "Database=ExoApi;Trusted_Connection=True;");
 
-                // Exemplo 1 de string de conexão:
-                // User ID = sa; Password = admin; Server = localhost; Database = ExoApi; -
-                // + Trusted_Connection=False;
-
-                // Exemplo 2 de string de conexão:
-                // Server = localhost\\SQLEXPRESS; Database = ExoApi; Trusted_Connection = True;
             }
         }
         public DbSet<Projeto> Projetos { get; set; }
